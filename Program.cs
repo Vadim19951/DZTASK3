4f04e2de43659fb10624c2aa922390ecc4852e6a
Vadim <uvolkov@374@gmail.com>
2022-06-28 18:12:12 +0300

    Add task3

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter your number ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a % 2 == 0)
+{
+    Console.WriteLine("Number " + a + " is Chetnoye number");
+}
+else
+{
+    Console.WriteLine("Number " + a + " is Nehetnoye number");
+}
